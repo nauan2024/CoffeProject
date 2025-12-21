ad00a889ffa9c35bdca898fcde5578affb1c3417
nauan2024 <nauanlaurentinotec@gmail.com>
2025-12-21 20:20:56 -0300

    feat:realized Main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -8,14 +8,14 @@ export const Main = () => {
                 <div className="content-text-main">
                     <span>We’ve got your morning covered with</span>
                     <h1>Coffee</h1>
-                    <p>It is best to start your day with a cup of coffee. Discover the
+                    <p>It is best to start your day with a cup of coffee.Discover the
                         best flavours coffee you will ever have. We provide the best
                         for our customers.</p>
                 </div>
-                
+
             </div>
 
-            <Button text={"Order Now"}/>
+            <Button text={"Order Now"} />
         </section>
     )
 }
