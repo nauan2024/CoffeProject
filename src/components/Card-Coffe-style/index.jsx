1c77ae30a558339d25ff213a22295cfa069a9c34
nauan2024 <nauanlaurentinotec@gmail.com>
2025-12-21 22:03:44 -0300

    feat:first card

--- src/components/Card-Coffe-style/index.jsx
+++ src/components/Card-Coffe-style/index.jsx
@@ -1,15 +1,17 @@
 import "./style.css"
 import { Button } from "../Button-order-now"
-export const Card_Coffe_style = ({ img, h3, tipo, preco,text2 }) => {
+export const Card_Coffe_style = ({ img, h3, tipo, preco, text2 }) => {
     return (
         <div className="card-coffe-style">
             <div className="content-card">
                 <img src={img} alt="" />
-                <h3>{h3}</h3>
-                <p>{tipo}</p>
-                <p>{preco}</p>
+                <div className="text-card">
+                    <h3>{h3}</h3>
+                    <p>{tipo}</p>
+                    <p className="price">{preco}</p>
+                </div>
             </div>
-            <Button text={text2}/>
+            <Button text={text2} />
         </div>
     )
 }
